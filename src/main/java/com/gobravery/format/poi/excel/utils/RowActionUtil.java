5f3d851fcde314e1f1e52dce94474d160d2de011
pc <pc@CS-T-Z0002>
2019-05-31 16:43:22 +0800

    增加like特性

--- src/main/java/com/gobravery/format/poi/excel/utils/RowActionUtil.java
+++ src/main/java/com/gobravery/format/poi/excel/utils/RowActionUtil.java
@@ -1,25 +1,19 @@
 package com.gobravery.format.poi.excel.utils;
 
 public class RowActionUtil {
-	public static boolean eval(String val1,String val2,String action){
-		//忽略全为空
-		if(empty(val1) && empty(val2)){
+	public static boolean eval(String val1, String val2, String action) {
+		// 忽略全为空
+		if (StringLike.empty(val1) && StringLike.empty(val2)) {
 			return false;
 		}
-		String a=val1;
-		String b=val2;
-		if(action.equals("%")){
-			//全匹配
-		}
-		//TODO %__
-		
+		String a = val1;
+		String b = val2;
 		//
-		if(a==b || a.equals(b)){
+		if (a == b || a.equals(b)) {
 			return true;
 		}
-		return false;
-	}
-	private static boolean empty(String v){
-		return v==null || v.equals("");
+		// %__
+		StringLike sl = new StringLike();
+		return sl.like(a, b, action);
 	}
 }
